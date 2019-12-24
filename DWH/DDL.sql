ff14c21933030bf606a10206f441a865779e16fc
vzaigrin <vzaigrin@yandex.ru>
2019-12-24 15:58:50 +0300

    Update DDL

--- DWH/DDL.sql
+++ DWH/DDL.sql
@@ -1,3 +1,5 @@
+#standardSQL
+
 -- Stage
 DROP TABLE IF EXISTS PP.STG_TAXI;
 CREATE TABLE PP.STG_TAXI (
@@ -10,8 +12,8 @@ CREATE TABLE PP.STG_TAXI (
     , ps_financing              FLOAT64 NOT NULL
     , taxi_financing            FLOAT64 NOT NULL
     , period_name               STRING NOT NULL
-    , filename					STRING NOT NULL
-    , load_ts					TIMESTAMP NOT NULL
+    , filename                  STRING NOT NULL
+    , load_ts                   TIMESTAMP NOT NULL
 )
 PARTITION BY _PARTITIONDATE
 ;
@@ -27,9 +29,9 @@ CREATE TABLE PP.STG_TELECOM (
     , payment_tariff            FLOAT64 NOT NULL
     , payment_ps                FLOAT64 NOT NULL
     , period_name               STRING NOT NULL
-    , filename					STRING NOT NULL
-    , load_ts					TIMESTAMP NOT NULL
-  
+    , filename                  STRING NOT NULL
+    , load_ts                   TIMESTAMP NOT NULL
+)
 PARTITION BY _PARTITIONDATE
 ;
 
@@ -44,8 +46,8 @@ CREATE TABLE PP.STG_CINEMA (
     , rrn                       STRING NOT NULL
     , card_number               STRING NOT NULL
     , period_name               STRING NOT NULL
-    , filename					STRING NOT NULL
-    , load_ts					TIMESTAMP NOT NULL
+    , filename                  STRING NOT NULL
+    , load_ts                   TIMESTAMP NOT NULL
 ) 
 PARTITION BY _PARTITIONDATE
 ;
@@ -61,8 +63,8 @@ CREATE TABLE PP.STG_RETAIL (
     , partner_financing         FLOAT64 NOT NULL
     , location                  STRING NOT NULL
     , period_name               STRING NOT NULL
-    , filename					STRING NOT NULL
-    , load_ts					TIMESTAMP NOT NULL
+    , filename                  STRING NOT NULL
+    , load_ts                   TIMESTAMP NOT NULL
 ) 
 PARTITION BY _PARTITIONDATE
 ;
@@ -80,8 +82,8 @@ CREATE TABLE PP.ODS_TAXI (
     , ps_financing              FLOAT64 NOT NULL
     , taxi_financing            FLOAT64 NOT NULL
     , period_name               STRING NOT NULL
-    , filename					STRING NOT NULL
-    , load_ts					TIMESTAMP NOT NULL
+    , filename                  STRING NOT NULL
+    , load_ts                   TIMESTAMP NOT NULL
 ) 
 PARTITION BY _PARTITIONDATE
 ;
@@ -97,8 +99,8 @@ CREATE TABLE PP.ODS_TELECOM (
     , payment_tariff            FLOAT64 NOT NULL
     , payment_ps                FLOAT64 NOT NULL
     , period_name               STRING NOT NULL
-    , filename					STRING NOT NULL
-    , load_ts					TIMESTAMP NOT NULL
+    , filename                  STRING NOT NULL
+    , load_ts                   TIMESTAMP NOT NULL
 ) 
 PARTITION BY _PARTITIONDATE
 ;
@@ -114,8 +116,8 @@ CREATE TABLE PP.ODS_CINEMA (
     , rrn                       STRING NOT NULL
     , card_number               STRING NOT NULL
     , period_name               STRING NOT NULL
-    , filename					STRING NOT NULL
-    , load_ts					TIMESTAMP NOT NULL
+    , filename                  STRING NOT NULL
+    , load_ts                   TIMESTAMP NOT NULL
 ) 
 PARTITION BY _PARTITIONDATE
 ;
@@ -131,8 +133,8 @@ CREATE TABLE PP.ODS_RETAIL (
     , partner_financing         FLOAT64 NOT NULL
     , location                  STRING NOT NULL
     , period_name               STRING NOT NULL
-    , filename					STRING NOT NULL
-    , load_ts					TIMESTAMP NOT NULL
+    , filename                  STRING NOT NULL
+    , load_ts                   TIMESTAMP NOT NULL
 ) 
 PARTITION BY _PARTITIONDATE
 ;
@@ -141,7 +143,7 @@ PARTITION BY _PARTITIONDATE
 -- DDS
 DROP TABLE IF EXISTS PP.HUB_PARTNERS;
 CREATE TABLE PP.HUB_PARTNERS (
-    partner_id                  NUMERIC NOT NULL
+    partner_id                  STRING NOT NULL
     , processed_dttm            TIMESTAMP NOT NULL
 ) 
 PARTITION BY _PARTITIONDATE
@@ -149,11 +151,11 @@ PARTITION BY _PARTITIONDATE
 
 DROP TABLE IF EXISTS PP.SUB_PARTNERS;
 CREATE TABLE PP.SUB_PARTNERS (
-    partner_id                  NUMERIC NOT NULL
+    partner_id                  STRING NOT NULL
     , partner_name              STRING NOT NULL
     , tag                       STRING NOT NULL
     , processed_dttm            TIMESTAMP NOT NULL
-    , hash                      INT64 NOT NULL
+    , _hash                     INT64 NOT NULL
     , version                   INT64 NOT NULL
 ) 
 PARTITION BY _PARTITIONDATE
@@ -161,7 +163,7 @@ PARTITION BY _PARTITIONDATE
 
 DROP TABLE IF EXISTS PP.HUB_DATA;
 CREATE TABLE PP.HUB_DATA (
-    data_id                     NUMERIC NOT NULL
+    data_id                     STRING NOT NULL
     , processed_dttm            TIMESTAMP NOT NULL
 ) 
 PARTITION BY _PARTITIONDATE
@@ -169,7 +171,7 @@ PARTITION BY _PARTITIONDATE
 
 DROP TABLE IF EXISTS PP.SUB_DATA;
 CREATE TABLE PP.SUB_DATA (
-    data_id                     NUMERIC NOT NULL
+    data_id                     STRING NOT NULL
     , card_number               STRING NOT NULL
     , card_is_premium           BOOL NOT NULL
     , operation_ts              TIMESTAMP NOT NULL
@@ -185,7 +187,7 @@ CREATE TABLE PP.SUB_DATA (
     , payment_partner           FLOAT64 NOT NULL
     , payment_other_client      FLOAT64 NOT NULL
     , processed_dttm            TIMESTAMP NOT NULL
-    , hash                      INT64 NOT NULL
+    , _hash                     INT64 NOT NULL
     , version                   INT64 NOT NULL
 ) 
 PARTITION BY _PARTITIONDATE
@@ -193,10 +195,10 @@ PARTITION BY _PARTITIONDATE
 
 DROP TABLE IF EXISTS PP.LNK_DATA_PARTNERS;
 CREATE TABLE PP.LNK_DATA_PARTNERS (
-    data_id                     NUMERIC NOT NULL
-    , partner_id                NUMERIC NOT NULL
+    data_id                     STRING NOT NULL
+    , partner_id                STRING NOT NULL
     , processed_dttm            TIMESTAMP NOT NULL
-    , hash                      INT64 NOT NULL
+    , _hash                     INT64 NOT NULL
     , version                   INT64 NOT NULL
 ) 
 PARTITION BY _PARTITIONDATE
@@ -204,10 +206,10 @@ PARTITION BY _PARTITIONDATE
 
 DROP TABLE IF EXISTS PP.LNK_DATA_BINS;
 CREATE TABLE PP.LNK_DATA_BINS (
-    data_id                     NUMERIC NOT NULL
-    , bin_id                    NUMERIC NOT NULL
+    data_id                     STRING NOT NULL
+    , bin_id                    STRING NOT NULL
     , processed_dttm            TIMESTAMP NOT NULL
-    , hash                      INT64 NOT NULL
+    , _hash                     INT64 NOT NULL
     , version                   INT64 NOT NULL
 ) 
 PARTITION BY _PARTITIONDATE
@@ -215,10 +217,10 @@ PARTITION BY _PARTITIONDATE
 
 DROP TABLE IF EXISTS PP.LNK_DATA_PRIVILEGES;
 CREATE TABLE PP.LNK_DATA_PRIVILEGES (
-    data_id                     NUMERIC NOT NULL
-    , privilege_id              NUMERIC NOT NULL
+    data_id                     STRING NOT NULL
+    , privilege_id              STRING NOT NULL
     , processed_dttm            TIMESTAMP NOT NULL
-    , hash                      INT64 NOT NULL
+    , _hash                     INT64 NOT NULL
     , version                   INT64 NOT NULL
 ) 
 PARTITION BY _PARTITIONDATE
@@ -226,7 +228,7 @@ PARTITION BY _PARTITIONDATE
 
 DROP TABLE IF EXISTS PP.HUB_BINS;
 CREATE TABLE PP.HUB_BINS (
-    bin_id                      NUMERIC NOT NULL
+    bin_id                      STRING NOT NULL
     , processed_dttm            TIMESTAMP NOT NULL
 ) 
 PARTITION BY _PARTITIONDATE
@@ -234,14 +236,14 @@ PARTITION BY _PARTITIONDATE
 
 DROP TABLE IF EXISTS PP.SUB_BINS;
 CREATE TABLE PP.SUB_BINS (
-    bin_id                      NUMERIC NOT NULL
+    bin_id                      STRING NOT NULL
     , bin                       STRING NOT NULL
     , range_from                INT64 NOT NULL
     , range_to                  INT64 NOT NULL
     , bank                      STRING NOT NULL
     , card_type                 STRING NOT NULL
     , processed_dttm            TIMESTAMP NOT NULL
-    , hash                      INT64 NOT NULL
+    , _hash                     INT64 NOT NULL
     , version                   INT64 NOT NULL
 ) 
 PARTITION BY _PARTITIONDATE
@@ -249,7 +251,7 @@ PARTITION BY _PARTITIONDATE
 
 DROP TABLE IF EXISTS PP.HUB_PRIVILEGES;
 CREATE TABLE PP.HUB_PRIVILEGES (
-    privilege_id                NUMERIC NOT NULL
+    privilege_id                STRING NOT NULL
     , processed_dttm            TIMESTAMP NOT NULL
 ) 
 PARTITION BY _PARTITIONDATE
@@ -257,12 +259,12 @@ PARTITION BY _PARTITIONDATE
 
 DROP TABLE IF EXISTS PP.SUB_PRIVILEGES;
 CREATE TABLE PP.SUB_PRIVILEGES (
-    privilege_id                NUMERIC NOT NULL
+    privilege_id                STRING NOT NULL
     , privilege_type            STRING NOT NULL
     , privilege_short           STRING NOT NULL
     , privilege_full            STRING NOT NULL
     , processed_dttm            TIMESTAMP NOT NULL
-    , hash                      INT64 NOT NULL
+    , _hash                     INT64 NOT NULL
     , version                   INT64 NOT NULL
 ) 
 PARTITION BY _PARTITIONDATE
@@ -297,7 +299,7 @@ CREATE TABLE PP.DM_REPORT (
 PARTITION BY _PARTITIONDATE
 ;
 
-CREATE OR REPLACE VIEW PP.V_DM_PARTNERS_CARD AS
+CREATE OR REPLACE VIEW `my-project-1530001957977.PP.V_DM_CARD` AS
 SELECT
     sum(trans_num) AS sum_opers
     , avg(payment_total) AS avg_total
@@ -305,12 +307,12 @@ SELECT
     , card_type
     , operation_day
     , period_name
-FROM PP.DM_PARTNERS p
+FROM `my-project-1530001957977.PP.DM_REPORT` p
 GROUP BY partner_name, card_type, operation_day, period_name
 ORDER BY partner_name, card_type, operation_day, period_name
 ;
 
-CREATE OR REPLACE VIEW PP.V_DM_PARTNERS_BANK AS
+CREATE OR REPLACE VIEW `my-project-1530001957977.PP.V_DM_BANK` AS
 SELECT
     sum(trans_num) AS sum_opers
     , avg(payment_total) AS avg_total
@@ -318,12 +320,12 @@ SELECT
     , bank
     , operation_day
     , period_name
-FROM PP.DM_PARTNERS p
+FROM `my-project-1530001957977.PP.DM_REPORT` p
 GROUP BY partner_name, bank, operation_day, period_name
 ORDER BY partner_name, bank, operation_day, period_name
 ;
 
-CREATE OR REPLACE VIEW PP.V_DM_PARTNERS_CITY AS
+CREATE OR REPLACE VIEW `my-project-1530001957977.PP.V_DM_CITY` AS
 SELECT
     sum(trans_num) AS sum_opers
     , avg(payment_total) AS avg_total
@@ -331,12 +333,12 @@ SELECT
     , operation_city
     , operation_day
     , period_name
-FROM PP.DM_PARTNERS p
+FROM `my-project-1530001957977.PP.DM_REPORT` p
 GROUP BY partner_name, operation_city, operation_day, period_name
 ORDER BY partner_name, operation_city, operation_day, period_name
 ;
 
-CREATE OR REPLACE VIEW PP.V_DM_PARTNERS_PURCHASE AS
+CREATE OR REPLACE VIEW `my-project-1530001957977.PP.V_DM_PURCHASE` AS
 SELECT
     sum(trans_num) AS sum_opers
     , sum(payment_total) AS sum_total
@@ -346,38 +348,300 @@ SELECT
     , purchase_type
     , operation_day
     , period_name
-FROM PP.DM_PARTNERS p
+FROM `my-project-1530001957977.PP.DM_REPORT` p
 GROUP BY partner_name, purchase_type, operation_day, period_name
 ORDER BY partner_name, purchase_type, operation_day, period_name
 ;
 
-CREATE OR REPLACE VIEW PP.V_DM_PARTNERS_PRIVILEGE AS
+CREATE OR REPLACE VIEW `my-project-1530001957977.PP.V_DM_PRIVILEGE` AS
 SELECT
     sum(trans_num) AS sum_opers
     , partner_name
     , privilege_type
     , operation_day
     , period_name
-FROM PP.DM_PARTNERS p
+FROM `my-project-1530001957977.PP.DM_REPORT` p
 GROUP BY partner_name, privilege_type, operation_day, period_name
 ORDER BY partner_name, privilege_type, operation_day, period_name
 ;
 
-CREATE OR REPLACE VIEW PP.V_DM_PARTNERS_PAYMENT AS
+CREATE OR REPLACE VIEW `my-project-1530001957977.PP.V_DM_PAYMENT` AS
 SELECT
-    avg(payment_main_client) AS client
+    avg(payment_main_client) AS avg_client
     , avg(payment_ps) AS ps
-    , avg(payment_partner) AS client
+    , avg(payment_partner) AS avg_partner
     , partner_name
     , operation_day
     , period_name
-FROM PP.DM_PARTNERS p
+FROM `my-project-1530001957977.PP.DM_REPORT` p
 GROUP BY partner_name, operation_day, period_name
 ORDER BY partner_name, operation_day, period_name
 ;
 
-CREATE OR REPLACE VIEW PP.V_DM_TAXI AS
+CREATE OR REPLACE VIEW `my-project-1530001957977.PP.V_DM_CARD_TAXI` AS
 SELECT
-FROM PP.DM_PARTNERS p
+    sum_opers
+    , avg_total
+    , card_type
+    , operation_day
+    , period_name
+FROM `my-project-1530001957977.PP.V_DM_CARD` p
 WHERE upper(p.partner_name) = 'TAXI'
+;
+
+CREATE OR REPLACE VIEW `my-project-1530001957977.PP.V_DM_BANK_TAXI` AS
+SELECT
+    sum_opers
+    , avg_total
+    , bank
+    , operation_day
+    , period_name
+FROM `my-project-1530001957977.PP.V_DM_BANK` p
+WHERE upper(p.partner_name) = 'TAXI'
+;
+
+CREATE OR REPLACE VIEW `my-project-1530001957977.PP.V_DM_CITY_TAXI` AS
+SELECT
+    sum_opers
+    , avg_total
+    , operation_city
+    , operation_day
+    , period_name
+FROM `my-project-1530001957977.PP.V_DM_CITY` p
+WHERE upper(p.partner_name) = 'TAXI'
+;
+
+CREATE OR REPLACE VIEW `my-project-1530001957977.PP.V_DM_PURCHASE_TAXI` AS
+SELECT
+    sum_opers
+    , sum_total
+    , sum_ps
+    , avg_total
+    , purchase_type
+    , operation_day
+    , period_name
+FROM `my-project-1530001957977.PP.V_DM_PURCHASE` p
+WHERE upper(p.partner_name) = 'TAXI'
+;
+
+CREATE OR REPLACE VIEW `my-project-1530001957977.PP.V_DM_PRIVILEGE_TAXI` AS
+SELECT
+    sum_opers
+    , privilege_type
+    , operation_day
+    , period_name
+FROM `my-project-1530001957977.PP.V_DM_PRIVILEGE` p
+WHERE upper(p.partner_name) = 'TAXI'
+;
+
+CREATE OR REPLACE VIEW `my-project-1530001957977.PP.V_DM_PAYMENT_TAXI` AS
+SELECT
+    avg_client
+    , ps
+    , avg_partner
+    , operation_day
+    , period_name
+FROM `my-project-1530001957977.PP.V_DM_PAYMENT` p
+WHERE upper(p.partner_name) = 'TAXI'
+;
+
+CREATE OR REPLACE VIEW `my-project-1530001957977.PP.V_DM_CARD_TELECOM` AS
+SELECT
+    sum_opers
+    , avg_total
+    , card_type
+    , operation_day
+    , period_name
+FROM `my-project-1530001957977.PP.V_DM_CARD` p
+WHERE upper(p.partner_name) = 'TELECOM'
+;
+
+CREATE OR REPLACE VIEW `my-project-1530001957977.PP.V_DM_BANK_TELECOM` AS
+SELECT
+    sum_opers
+    , avg_total
+    , bank
+    , operation_day
+    , period_name
+FROM `my-project-1530001957977.PP.V_DM_BANK` p
+WHERE upper(p.partner_name) = 'TELECOM'
+;
+
+CREATE OR REPLACE VIEW `my-project-1530001957977.PP.V_DM_CITY_TELECOM` AS
+SELECT
+    sum_opers
+    , avg_total
+    , operation_city
+    , operation_day
+    , period_name
+FROM `my-project-1530001957977.PP.V_DM_CITY` p
+WHERE upper(p.partner_name) = 'TELECOM'
+;
+
+CREATE OR REPLACE VIEW `my-project-1530001957977.PP.V_DM_PURCHASE_TELECOM` AS
+SELECT
+    sum_opers
+    , sum_total
+    , sum_ps
+    , avg_total
+    , purchase_type
+    , operation_day
+    , period_name
+FROM `my-project-1530001957977.PP.V_DM_PURCHASE` p
+WHERE upper(p.partner_name) = 'TELECOM'
+;
+
+CREATE OR REPLACE VIEW `my-project-1530001957977.PP.V_DM_PRIVILEGE_TELECOM` AS
+SELECT
+    sum_opers
+    , privilege_type
+    , operation_day
+    , period_name
+FROM `my-project-1530001957977.PP.V_DM_PRIVILEGE` p
+WHERE upper(p.partner_name) = 'TELECOM'
+;
+
+CREATE OR REPLACE VIEW `my-project-1530001957977.PP.V_DM_PAYMENT_TELECOM` AS
+SELECT
+    avg_client
+    , ps
+    , avg_partner
+    , operation_day
+    , period_name
+FROM `my-project-1530001957977.PP.V_DM_PAYMENT` p
+WHERE upper(p.partner_name) = 'TELECOM'
+;
+
+CREATE OR REPLACE VIEW `my-project-1530001957977.PP.V_DM_CARD_CINEMA` AS
+SELECT
+    sum_opers
+    , avg_total
+    , card_type
+    , operation_day
+    , period_name
+FROM `my-project-1530001957977.PP.V_DM_CARD` p
+WHERE upper(p.partner_name) = 'CINEMA'
+;
+
+CREATE OR REPLACE VIEW `my-project-1530001957977.PP.V_DM_BANK_CINEMA` AS
+SELECT
+    sum_opers
+    , avg_total
+    , bank
+    , operation_day
+    , period_name
+FROM `my-project-1530001957977.PP.V_DM_BANK` p
+WHERE upper(p.partner_name) = 'CINEMA'
+;
+
+CREATE OR REPLACE VIEW `my-project-1530001957977.PP.V_DM_CITY_CINEMA` AS
+SELECT
+    sum_opers
+    , avg_total
+    , operation_city
+    , operation_day
+    , period_name
+FROM `my-project-1530001957977.PP.V_DM_CITY` p
+WHERE upper(p.partner_name) = 'CINEMA'
+;
+
+CREATE OR REPLACE VIEW `my-project-1530001957977.PP.V_DM_PURCHASE_CINEMA` AS
+SELECT
+    sum_opers
+    , sum_total
+    , sum_ps
+    , avg_total
+    , purchase_type
+    , operation_day
+    , period_name
+FROM `my-project-1530001957977.PP.V_DM_PURCHASE` p
+WHERE upper(p.partner_name) = 'CINEMA'
+;
+
+CREATE OR REPLACE VIEW `my-project-1530001957977.PP.V_DM_PRIVILEGE_CINEMA` AS
+SELECT
+    sum_opers
+    , privilege_type
+    , operation_day
+    , period_name
+FROM `my-project-1530001957977.PP.V_DM_PRIVILEGE` p
+WHERE upper(p.partner_name) = 'CINEMA'
+;
+
+CREATE OR REPLACE VIEW `my-project-1530001957977.PP.V_DM_PAYMENT_CINEMA` AS
+SELECT
+    avg_client
+    , ps
+    , avg_partner
+    , operation_day
+    , period_name
+FROM `my-project-1530001957977.PP.V_DM_PAYMENT` p
+WHERE upper(p.partner_name) = 'CINEMA'
+;
+
+CREATE OR REPLACE VIEW `my-project-1530001957977.PP.V_DM_CARD_RETAIL` AS
+SELECT
+    sum_opers
+    , avg_total
+    , card_type
+    , operation_day
+    , period_name
+FROM `my-project-1530001957977.PP.V_DM_CARD` p
+WHERE upper(p.partner_name) = 'RETAIL'
+;
+
+CREATE OR REPLACE VIEW `my-project-1530001957977.PP.V_DM_BANK_RETAIL` AS
+SELECT
+    sum_opers
+    , avg_total
+    , bank
+    , operation_day
+    , period_name
+FROM `my-project-1530001957977.PP.V_DM_BANK` p
+WHERE upper(p.partner_name) = 'RETAIL'
+;
+
+CREATE OR REPLACE VIEW `my-project-1530001957977.PP.V_DM_CITY_RETAIL` AS
+SELECT
+    sum_opers
+    , avg_total
+    , operation_city
+    , operation_day
+    , period_name
+FROM `my-project-1530001957977.PP.V_DM_CITY` p
+WHERE upper(p.partner_name) = 'RETAIL'
+;
+
+CREATE OR REPLACE VIEW `my-project-1530001957977.PP.V_DM_PURCHASE_RETAIL` AS
+SELECT
+    sum_opers
+    , sum_total
+    , sum_ps
+    , avg_total
+    , purchase_type
+    , operation_day
+    , period_name
+FROM `my-project-1530001957977.PP.V_DM_PURCHASE` p
+WHERE upper(p.partner_name) = 'RETAIL'
+;
+
+CREATE OR REPLACE VIEW `my-project-1530001957977.PP.V_DM_PRIVILEGE_RETAIL` AS
+SELECT
+    sum_opers
+    , privilege_type
+    , operation_day
+    , period_name
+FROM `my-project-1530001957977.PP.V_DM_PRIVILEGE` p
+WHERE upper(p.partner_name) = 'RETAIL'
+;
+
+CREATE OR REPLACE VIEW `my-project-1530001957977.PP.V_DM_PAYMENT_RETAIL` AS
+SELECT
+    avg_client
+    , ps
+    , avg_partner
+    , operation_day
+    , period_name
+FROM `my-project-1530001957977.PP.V_DM_PAYMENT` p
+WHERE upper(p.partner_name) = 'RETAIL'
 ;
