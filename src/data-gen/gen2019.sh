15a627721c7d44b1fab26366806ad727008bf3eb
vzaigrin <vzaigrin@yandex.ru>
2020-01-13 16:59:58 +0300

    Update data 2019

--- src/data-gen/gen2019.sh
+++ src/data-gen/gen2019.sh
@@ -4,11 +4,9 @@ basedir=../../data/2019
 for file in templates/*
 do
     partner=`basename ${file} .json`
-    mkdir -p $basedir/hist/$partner
-    mkdir -p $basedir/12/$partner
-    for num in {1..11}
+    mkdir -p $basedir/$partner
+    for num in {1..12}
     do
-        ./data-gen -t $file -o $basedir/hist/$partner/$partner-${num}.csv -y 2019 -m $num -e 10 -v
+        ./data-gen -t $file -o $basedir/$partner/$partner-${num}.csv -y 2019 -m $num -n 1000000 -e 10 -v
     done
-    ./data-gen -t $file -o $basedir/12/$partner/$partner-12.csv -y 2019 -m 12 -e 10 -v
 done
