7f3f6a43bbd823e9c67d5c6b52d3dd4444660e4c
vzaigrin <vzaigrin@yandex.ru>
2020-02-06 21:47:48 +0300

    Update README

---
+++ DWH/Partners/add_all.sql
@@ -0,0 +1,5 @@
+DECLARE result STRING;
+CALL PP.ADD_PARTNER ('cinema', 'Cinema', result);
+CALL PP.ADD_PARTNER ('retail', 'Retail', result);
+CALL PP.ADD_PARTNER ('taxi', 'Taxi', result);
+CALL PP.ADD_PARTNER ('telecom', 'Telecom', result);
